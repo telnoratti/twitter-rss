400610fddb92f4f2b2891eff896f5e17ad92edcf
Calvin Winkowski <telnoratti@gmail.com>
2013-07-30 15:50:17 -0400

    Change path joins to use python's os.path module.

--- run.py
+++ run.py
@@ -6,21 +6,22 @@ import time
 import subprocess
 import config
 import sys
+import os
 
 # Launch web server
-p = subprocess.Popen([sys.executable, config.INSTALL_DIR + 'server.py'])
+p = subprocess.Popen([sys.executable, os.path.join(config.INSTALL_DIR + 'server.py')])
 
 # Update the feeds
 try:
     while 1:
         print('Updating ALL THE FEEDS!')
         try:
-            with open(config.XML_DIR + 'user/user.txt', 'r') as usernames:
+            with open(os.path.join(config.XML_DIR, 'user/user.txt'), 'r') as usernames:
                 for user in usernames:
                     twitter_rss.UserTweetGetter(user)
             usernames.close()
 
-            with open(config.XML_DIR + 'htag/htag.txt', 'r') as hashtags:
+            with open(os.path.join(config.XML_DIR + 'htag/htag.txt'), 'r') as hashtags:
                 for htag in hashtags:
                     twitter_rss.HashtagTweetGetter(user)
             hashtags.close()
